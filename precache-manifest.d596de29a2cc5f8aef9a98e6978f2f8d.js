fbf67c0dc5af07ba7631fd518c44e74bc8658d54
himans17 <kushwahahimanshu18@gmail.com>
2024-04-16 15:42:39 +0530

    Updates

---
+++ precache-manifest.d596de29a2cc5f8aef9a98e6978f2f8d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9b5791b89a8237c3467374c981f37c0",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "973fc26f7811140d7c36",
+    "url": "/reactportfolio/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "a2f4be923c966c6db31e",
+    "url": "/reactportfolio/static/css/main.91c8931a.chunk.css"
+  },
+  {
+    "revision": "973fc26f7811140d7c36",
+    "url": "/reactportfolio/static/js/2.4ceae23c.chunk.js"
+  },
+  {
+    "revision": "29fe86816a802490cec5",
+    "url": "/reactportfolio/static/js/3.d3e4d6ea.chunk.js"
+  },
+  {
+    "revision": "a2f4be923c966c6db31e",
+    "url": "/reactportfolio/static/js/main.fcf43a58.chunk.js"
+  },
+  {
+    "revision": "62e2e34e058038f6f6cd",
+    "url": "/reactportfolio/static/js/runtime~main.23d77406.js"
+  },
+  {
+    "revision": "df5657fc387326880ea38dbb0529a1a1",
+    "url": "/reactportfolio/static/media/HIMANSHU-KUSHWAH.df5657fc.pdf"
+  },
+  {
+    "revision": "fc2371abacf5fc7ca26bb306c6afcf61",
+    "url": "/reactportfolio/static/media/a1cashloans.fc2371ab.png"
+  },
+  {
+    "revision": "7f9be4e9c8ef27e162167ddf427c5c42",
+    "url": "/reactportfolio/static/media/atom.7f9be4e9.png"
+  },
+  {
+    "revision": "b23365106d3bbcd664db46fe6517124b",
+    "url": "/reactportfolio/static/media/bootstrap.b2336510.png"
+  },
+  {
+    "revision": "92e0187130c13acffaedb914f5b57362",
+    "url": "/reactportfolio/static/media/cismycro.92e01871.png"
+  },
+  {
+    "revision": "b848d5d9bb221592064de0f356f61676",
+    "url": "/reactportfolio/static/media/css-3.b848d5d9.png"
+  },
+  {
+    "revision": "bef8eca1168bd7039d8d519e43015705",
+    "url": "/reactportfolio/static/media/git.bef8eca1.png"
+  },
+  {
+    "revision": "7068dab81775fae8fd899de730407900",
+    "url": "/reactportfolio/static/media/hero-back.7068dab8.png"
+  },
+  {
+    "revision": "d9de75e5bf3a7b0ef2a48321acc06e88",
+    "url": "/reactportfolio/static/media/java-script.d9de75e5.png"
+  },
+  {
+    "revision": "5ffb9ff70c69224e650148a9d3f70ff2",
+    "url": "/reactportfolio/static/media/laptop-screen.5ffb9ff7.png"
+  },
+  {
+    "revision": "f634f644829236e43bdd6d2efa62fe6c",
+    "url": "/reactportfolio/static/media/login.f634f644.png"
+  },
+  {
+    "revision": "87f3548aab4360628f6d9f06bd0e78a7",
+    "url": "/reactportfolio/static/media/makemypolicy.87f3548a.png"
+  },
+  {
+    "revision": "01d4eb4a91cf2778c3bb185aac400a02",
+    "url": "/reactportfolio/static/media/maxmoneymart.01d4eb4a.png"
+  },
+  {
+    "revision": "a3ef4731b1f9cf7d935941a356674c50",
+    "url": "/reactportfolio/static/media/sefoods.a3ef4731.png"
+  },
+  {
+    "revision": "dc2d9cd64cb5f0f7d0d3cc6d7093b3e7",
+    "url": "/reactportfolio/static/media/typing.dc2d9cd6.png"
+  },
+  {
+    "revision": "e30d601edd8bc9ff05dee75a4b087066",
+    "url": "/reactportfolio/static/media/wekcare.e30d601e.png"
+  },
+  {
+    "revision": "db4298ab4dbd04c9e9a2395533a01082",
+    "url": "/reactportfolio/static/media/wordpress.db4298ab.png"
+  }
+]);
